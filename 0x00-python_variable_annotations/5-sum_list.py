28426b486c70d95dd0653c299072ae93de42bc48
Magdalina1 <majdalol95@gmail.com>
2024-04-19 19:20:49 +0000

    5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 '''Task 5's module.'''
-from typing import list
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
-    '''computes the sum of a list of floating-point numbers.'''
+def sum_list(input_list: List[float]) -> float:
+    '''Computes the sum of a list of floating-point numbers.'''
     return float(sum(input_list))
