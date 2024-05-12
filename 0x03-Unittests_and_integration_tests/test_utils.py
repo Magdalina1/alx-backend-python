62f5ba36294e7527ac84d253a8aeb0448c2f323b
Magdalina1 <majdalol95@gmail.com>
2024-05-12 12:21:30 +0000

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -50,3 +50,9 @@ class TestAccessNestedMap(unittest.TestCase):
             ({"a": 1}, ("a", "b"), KeyError)
         ]
     )
+    def test_access_nested_map_exception(self, nested_map, path,
+                                         expected_output):
+        """_summary_
+        """
+        with self.assertRaises(expected_output) as context:
+            access_nested_map(nested_map, path)
