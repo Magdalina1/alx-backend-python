05b7c2fab6785dce80823ecece7afddb35d98a53
Magdalina1 <majdalol95@gmail.com>
2024-05-12 11:25:51 +0000

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,48 +1,52 @@
 #!/usr/bin/env python3
-"""A module for testing the client molude."""
+"""Familiarize yourself with the utils.access_nested_map function and
+understand its purpose. Play with it in the Python console to make
+sure you understand.
+
+In this task you will write the first unit test for utils.access_nested_map.
+
+Create a TestAccessNestedMap class that inherits from unittest.TestCase.
+
+Implement the TestAccessNestedMap.test_access_nested_map method to test that
+the method returns what it is supposed to.
+
+Decorate the method with @parameterized.expand to test the function for
+ollowing inputs:
+
+nested_map={"a": 1}, path=("a",)
+nested_map={"a": {"b": 2}}, path=("a",)
+nested_map={"a": {"b": 2}}, path=("a", "b")
+"""
+
 import unittest
-from typing import dict
-from unittest.mock import (
-    MagicMock,
-    Mock,
-    PropertyMock,
-    patch,
-)
-from parameterized import parameterized, parameterized_class
-from requests import HTTPError
-
-from client import (
-    GithubOrgClient
-)
-from fixtures import TEST_PAYLOAD
+from parameterized import parameterized
+from utils import access_nested_map, get_json, memoize
+from unittest.mock import patch, Mock
 
 
-class TestGithubOrgClient(unittest.TestCase):
+class TestAccessNestedMap(unittest.TestCase):
     """_summary_
 
     Args:
-            unittest (_type_): _description_
+        unittest (_type_): _description_
     """
-    @parameterized.expand([
-        ("google", {'login': "google"}),
-        ("abc", {'login': "abc"}),
-    ])
-    @patch(
-        "client.get_json",
-    )
-    def test_org(self, org: str, expected_response: Dict,
-                 mocked_function: MagicMock) -> None:
-        """_summary_
 
-        Args:
-                org (str): _description_
-                expected_response (Dict): _description_
-                mocked_function (MagicMock): _description_
+    @parameterized.expand(
+        [
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2)
+        ]
+    )
+    def test_access_nested_map(self, nested_map, path, expected_output):
+        """_summary_
         """
-        mocked_function.return_value = MagicMock(
-            return_value=expected_response)
-        goclient = GithubOrgClient(org)
-        self.assertEqual(goclient.org(), expected_response)
-        mocked_function.assert_called_once_with(
-            "https://api.github.com/orgs/{}".format(org)
-            )
+        result = access_nested_map(nested_map, path)
+        self.assertEqual(result, expected_output)
+
+    @parameterized.expand(
+        [
+            ({}, ("a",), KeyError),
+            ({"a": 1}, ("a", "b"), KeyError)
+        ]
+    )
